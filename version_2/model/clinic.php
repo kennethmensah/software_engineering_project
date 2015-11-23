7ea6a49a318a423b232a2bd338a08f04f3eb4eff
kennethmensah <kenneth.mensah@ashesi.edu.gh>
2015-11-23 18:25:17 +0000

    added new database and model classes for tasks and clinics

---
+++ version_2/model/clinic.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: StreetHustling
+ * Date: 11/23/15
+ * Time: 6:23 PM
+ */
+
+include_once 'adb.php';
+
+class clinic extends adb{
+
+    function clinic(){
+
+    }
+}
--- version_2/model/nurse_task_managerV2.sql
+++ version_2/model/nurse_task_managerV2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 22, 2015 at 01:15 AM
+-- Generation Time: Nov 23, 2015 at 07:15 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -31,7 +31,62 @@ CREATE TABLE IF NOT EXISTS `se_admin` (
   `fname` varchar(50) NOT NULL,
   `sname` varchar(50) NOT NULL,
   `district` int(11) NOT NULL,
-  `phone` varchar(50) NOT NULL
+  `phone` varchar(50) NOT NULL,
+  `gender` enum('M','F') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `se_admin`
+--
+
+INSERT INTO `se_admin` (`admin_id`, `fname`, `sname`, `district`, `phone`, `gender`) VALUES
+(1, 'Kwakye', 'Davidson', 3, '+233200393945', 'M'),
+(4, 'Mensah', 'Kenneth Mintah', 2, ' 233200393945', 'M');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `se_clinics`
+--
+
+CREATE TABLE IF NOT EXISTS `se_clinics` (
+`clinic_id` int(11) NOT NULL,
+  `clinic_name` varchar(100) NOT NULL,
+  `clinic_location` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `se_clinic_tasks`
+--
+
+CREATE TABLE IF NOT EXISTS `se_clinic_tasks` (
+`task_id` int(11) NOT NULL,
+  `task_title` varchar(50) NOT NULL,
+  `task_desc` varchar(300) NOT NULL,
+  `assigned_to` int(11) NOT NULL,
+  `assigned_by` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `date_completed` date NOT NULL,
+  `due_date` date NOT NULL,
+  `due_time` time NOT NULL,
+  `confirmed` enum('confirmed','not') NOT NULL,
+  `clinic` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `se_district_tasks`
+--
+
+CREATE TABLE IF NOT EXISTS `se_district_tasks` (
+`task_id` int(11) NOT NULL,
+  `task_title` varchar(50) NOT NULL,
+  `task_desc` varchar(300) NOT NULL,
+  `clinics` varchar(200) NOT NULL,
+  `due_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -45,8 +100,18 @@ CREATE TABLE IF NOT EXISTS `se_nurses` (
   `fname` varchar(50) NOT NULL,
   `sname` varchar(50) NOT NULL,
   `district_zone` int(11) NOT NULL,
-  `phone` varchar(50) NOT NULL
+  `phone` varchar(50) NOT NULL,
+  `gender` enum('M','F') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `se_nurses`
+--
+
+INSERT INTO `se_nurses` (`nurse_id`, `fname`, `sname`, `district_zone`, `phone`, `gender`) VALUES
+(1, 'Araba', 'Maison', 2, '+233244393945', 'M'),
+(6, 'Baddoo', 'Edwina', 2147483647, '1', 'F'),
+(7, 'Baddoo', 'Edwina', 1, ' 233200393945', 'F');
 
 -- --------------------------------------------------------
 
@@ -59,8 +124,17 @@ CREATE TABLE IF NOT EXISTS `se_supervisors` (
   `fname` varchar(50) NOT NULL,
   `sname` varchar(50) NOT NULL,
   `district_zone` int(11) NOT NULL,
-  `phone` varchar(50) NOT NULL
+  `phone` varchar(50) NOT NULL,
+  `gender` enum('M','F') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `se_supervisors`
+--
+
+INSERT INTO `se_supervisors` (`supervisor_id`, `fname`, `sname`, `district_zone`, `phone`, `gender`) VALUES
+(1, 'Aelaf', 'Dafla', 2, '+233200393945', 'M'),
+(8, 'Edwina', 'Baddoo', 1, ' 233200393945', 'F');
 
 -- --------------------------------------------------------
 
@@ -74,7 +148,7 @@ CREATE TABLE IF NOT EXISTS `se_users` (
   `user_type` enum('admin','nurse','supervisor') NOT NULL,
 `user_id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `se_users`
@@ -82,11 +156,54 @@ CREATE TABLE IF NOT EXISTS `se_users` (
 
 INSERT INTO `se_users` (`username`, `password`, `user_type`, `user_id`, `email`) VALUES
 ('admin', '6e8204c0862ec8abecb49762f0899554', 'admin', 2, '0'),
-('kwasi_ansah', 'demo', 'admin', 1, '0');
+('amakye.dede', 'amakye.dede', 'nurse', 9, 'aben.woha'),
+('edwina.baddoo', '3752ae0c5aef2c5de20eafc721567a9b', 'nurse', 6, 'edwina.baddoo@gmail.com'),
+('edwina1.baddoo', '3752ae0c5aef2c5de20eafc721567a9b', 'nurse', 7, 'edwina.baddoo@gmail.com'),
+('edwina2.baddoo', '3752ae0c5aef2c5de20eafc721567a9b', 'supervisor', 8, 'edwina.baddoo@gmail.com'),
+('kenneth.mensah', '6e8204c0862ec8abecb49762f0899554', 'admin', 4, 'kenneth.mensah@gmail.com'),
+('KwameAnim', 'Ajaie', 'admin', 10, 'k.a@sth.com'),
+('kwasi_ansah', 'demo', 'admin', 1, '0'),
+('kwasi_bansah', 'demo', 'admin', 3, '');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `se_admin`
+--
+ALTER TABLE `se_admin`
+ ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `se_clinics`
+--
+ALTER TABLE `se_clinics`
+ ADD PRIMARY KEY (`clinic_id`);
+
+--
+-- Indexes for table `se_clinic_tasks`
+--
+ALTER TABLE `se_clinic_tasks`
+ ADD PRIMARY KEY (`task_id`);
+
+--
+-- Indexes for table `se_district_tasks`
+--
+ALTER TABLE `se_district_tasks`
+ ADD PRIMARY KEY (`task_id`);
+
+--
+-- Indexes for table `se_nurses`
+--
+ALTER TABLE `se_nurses`
+ ADD PRIMARY KEY (`nurse_id`);
+
+--
+-- Indexes for table `se_supervisors`
+--
+ALTER TABLE `se_supervisors`
+ ADD PRIMARY KEY (`supervisor_id`);
 
 --
 -- Indexes for table `se_users`
@@ -99,10 +216,25 @@ ALTER TABLE `se_users`
 --
 
 --
+-- AUTO_INCREMENT for table `se_clinics`
+--
+ALTER TABLE `se_clinics`
+MODIFY `clinic_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `se_clinic_tasks`
+--
+ALTER TABLE `se_clinic_tasks`
+MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `se_district_tasks`
+--
+ALTER TABLE `se_district_tasks`
+MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `se_users`
 --
 ALTER TABLE `se_users`
-MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
