cbfbc8d66b2c0db94782db9593779f623ebec296
norbertsackey <norbertsackey@gmail.com>
2015-11-24 22:10:01 +0000

    added clinic model, district model and view

--- version_2/model/district_task.php
+++ version_2/model/district_task.php
@@ -10,7 +10,98 @@ include_once 'adb.php';
 
 class district_task extends adb{
 
-    function district_task(){
-
+  
+    /*
+    This is a constructor for the district_task class 
+    */
+    function district_task()
+    {
+        
     }
+    
+    /**
+    * This function adds a new district_task given the required parameters
+    * 
+    *@param int $id this represents the unique identifier for each district task
+    *@param String $taskTitle this is the title given to the task 
+    *@param String $taskDes this is description for all activities involved in the task
+    *@param String $clinics this represents the names one or more clinics assigned this task
+    *@param String $date this is the date on which the new task was added  
+    *@return bool the result will return true/false whether the sql query is successful
+    */
+    
+    function add_district_task($taskTitle, $taskDesc, $clinics, $date)
+    {
+        
+        $str_query = "insert into se_district_tasks set " . " task_title = '$taskTitle'," . "task_desc = '$taskDesc',". "clinics = '$clinics'," . "due_date = '$date'";
+        return $this->query($str_query);
+    }
+    
+    /**
+    * This function updates an existing district_task given the required parameters
+    *
+    *@param int $id this represents the unique identifier for each district task
+    *@param String $taskTitle this is the title given to the task 
+    *@param String $taskDes this is description for all activities involved in the task
+    *@param String $clinics this represents the names one or more clinics assigned this task
+    *@param String $date this is the date on which the new task was added  
+    *@return bool the result will return true/false whether the sql query is successful
+    */
+    function edit_district_task($taskTitle, $taskDesc, $clinics,$date)
+    {
+        $str_query = "update se_district_tasks set " .  "task_title = '$taskTitle'," . "task_desc = '$taskDesc'". "clinics = '$clinics'," . "due_date = '$date'". "where task_id = '$id'";
+        return $this->query($str_query);
+    }
+    
+    
+    /**
+    *This function retrives the information for a given district task using its id
+    *@param int $id this represents the unique identifier for each district task
+    *@return bool the result will return true/false whether the sql query is successful
+    */
+    function get_district_task($id)
+    {
+        $str_query = "select task_title, task_desc,clinics,due_date from se_district_tasks
+                where task_id = $id";
+        return $this->query($str_query);
+    }
+    
+    /**
+    *This function adds retrives the information for all district_tasks stored in the database
+    *@return bool the result will return true/false whether the sql query is successful
+    */
+
+    function get_district_tasks()
+    {
+        $str_query = "select task_id, task_title, task_desc,clinics,due_date from se_district_tasks";
+        
+        return $this->query($str_query);
+    }
+
+    /**
+    *This function adds deletes the row storing data for a given district task using its id
+    *@param int $id this represents the unique identifier for each district task
+    *@return bool the result will return true/false whether the sql query is successful
+    */
+    
+    function delete_district_task($id)
+    {
+        $str_query = "delete from  se_district_tasks where task_id = $id";
+        return $this->query($str_query);
+    }
+    
+    /**
+    *This function adds searches for the rows with names of district_tasks that match the pattern
+    *@param int $sn this represents the name of the district_task
+    *@return bool the result will return true/false whether the sql query is successful
+    */
+    
+    function search_district_task_by_name($sn)
+    {
+        $str_query = "select task_id, task_title, task_desc,clinics,due_date from se_district_tasks where task_name like '%$sn%'";
+        return $this->query($str_query);
+    }
+    
 }
+
+?>
---
+++ version_2/view/add_district_task_view.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+    <?php
+
+if (isset($_REQUEST['tt']) && isset($_REQUEST['td']) && isset($_REQUEST['cli']) && isset($_REQUEST['dt'])) {
+    include("district_task.php");
+    $obj = new district_task();
+    
+
+    $task_title   = $_REQUEST['tt'];
+    $task_desc    = $_REQUEST['td'];
+    $clinics   = $_REQUEST['cli'];
+    $date = $_REQUEST['dt'];
+
+    $temp = strtotime($date);
+    $end_date = date('Y-m-d', $temp);
+   
+    
+    if (!$obj->add_district_task($task_title, $task_desc, $clinics, $end_date)) {
+        echo "Error adding " . mysql_error();
+    } else {
+        echo "Added $task_title, $task_desc, $clinics, $end_date";
+    }
+}
+?>
+       <div class="container" id="form">
+            <form action="add_district_task.php" method="get">
+                <div>Task Title: <input type="text" name="tt"></div>
+                <div>Task Description: <textarea name="td"></textarea></div>
+                <div>Clinics: <input type="text" name="cli"></div>
+                <div>Date: <input type="text" name="dt"></div>
+                <div><input type="submit" value="Save" onsubmit = "confirmAdd()"></div>
+
+            </form>
+        </div>
+    </body>
+</html>
+?>
