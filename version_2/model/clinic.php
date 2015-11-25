0ad677221155e79bfd3441f60662dc55dff374e9
kennethmensah <kenneth.mensah@ashesi.edu.gh>
2015-11-25 09:03:43 +0000

    added function for adding nurse

--- version_2/model/clinic.php
+++ version_2/model/clinic.php
@@ -17,13 +17,25 @@ class clinic extends adb{
 
     }
 
-    function add_clinic(){
-        $str_query =  "INSERT into se_users SET
-                   username = '$username',
-                   password = '$password',
-                   email = '$email',
-                   user_type = '$user_type'";
+    /**
+     * @param $clinic_name
+     * @param $clinic_location
+     * @return bool
+     */
+    function add_clinic($clinic_name, $clinic_location){
+        $str_query =  "INSERT into se_clinics SET
+                   clinic_name = '$clinic_name',
+                   clinic_location = '$clinic_location'";
 
         return $this->query($str_query);
     }
+
+
 }
+
+/**
+ * Unit test and usage
+ */
+
+$obj = new clinic();
+$obj->add_clinic('Kwashieman Community Clinic', 'Kwashieman');
