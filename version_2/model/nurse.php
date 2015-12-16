6c8ecbaac3d4c2166cb7024f1cbf1972bcdc436a
josh-ahey <josh.ahey@gmail.com>
2015-12-16 22:50:38 +0000

    Comment
    Commented on the nurse.php page

--- version_2/model/nurse.php
+++ version_2/model/nurse.php
@@ -10,15 +10,19 @@ include_once 'adb.php';
 
 class nurses extends adb{
 
+    /*
+     *@constructor  nurses() Constructor for the nurses class 
+    */
     function nurses(){}
 
     /**
-     * @param $nurses_id
-     * @param $fname
-     * @param $sname
-     * @param $district_zone
-     * @param $phone
-     * @return bool
+     * @method boolean add_nurses() add_nurses($nurse_id, $fname, $sname, $district_zone, $phone,$gender) Adds nurses to the database
+     * @param int $nurses_id The nurse ID
+     * @param string $fname The firstname
+     * @param string $sname The lastname
+     * @param string $district_zone The District Zone
+     * @param string $phone The phone number
+     * @return boolean
      */
     function add_nurses($nurse_id, $fname, $sname, $district_zone, $phone,$gender){
         $str_query =  "INSERT into se_nurses SET
@@ -34,12 +38,13 @@ class nurses extends adb{
 
 
     /**
-     * @param $nurses_id
-     * @param $fname
-     * @param $sname
-     * @param $district_zone
-     * @param $phone
-     * @return bool
+     * @method boolean update_nurses_details() update_nurses_details($nurse_id, $fname, $sname, $district_zone, $phone, $gender) Updates nurse details
+     * @param int $nurses_id The nurse ID
+     * @param string $fname Nurse firstname
+     * @param string $sname Nurse lastname
+     * @param string $district_zone The district zone
+     * @param string $phone The phone number
+     * @return boolean
      */
     function update_nurses_details($nurse_id, $fname, $sname, $district_zone, $phone, $gender){
         $str_query = "UPDATE se_nurses SET
@@ -54,9 +59,10 @@ class nurses extends adb{
     }
 
     /**
-     * @param $nurses_id
-     * @param $district_zone
-     * @return bool
+     * @method boolean update_district_zone() update_district_zone($nurse_id, $district_zone) Updates nurses ID and District Zone
+     * @param int $nurses_id The nurse ID
+     * @param string $district_zone The district zone
+     * @return boolean
      */
     function update_district_zone($nurse_id, $district_zone){
         $str_query = "UPDATE se_nurses SET
@@ -67,9 +73,10 @@ class nurses extends adb{
     }
 
     /**
-     * @param $nurses_id
-     * @param $phone
-     * @return bool
+     * @method boolean update_phone() update_phone($nurse_id, $phone) Update Phone number for nurse
+     * @param int $nurses_id The nurse ID
+     * @param string $phone The phone number
+     * @return boolean
      */
     function update_phone($nurse_id, $phone){
         $str_query = "UPDATE se_nurses SET
@@ -81,8 +88,9 @@ class nurses extends adb{
 
 
     /**
-     * @param $nurses_id
-     * @return bool
+     * @method boolean get_details() get_details($nurse_id) Getshe details of a nurse
+     * @param int $nurses_id The nurse ID
+     * @return boolean
      */
     function get_details($nurse_id){
         $str_query = "SELECT * FROM se_nurses
